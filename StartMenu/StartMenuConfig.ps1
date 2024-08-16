f73e11eaeafc1f4e13d144fb197d9c360ca36356
Billabong1985 <124009245+Billabong1985@users.noreply.github.com>
2024-08-16 15:54:27 +0100

    Update StartMenuConfig.ps1

--- StartMenu/StartMenuConfig.ps1
+++ StartMenu/StartMenuConfig.ps1
@@ -2,10 +2,8 @@
 #                                                                 #
 # Configures Windows start menu layouts by editing registry       #
 #                                                                 #
-# Registry keys are built into an array with pscustomobject       #
-#                                                                 #
-# Additional keys can be added to the config by copy/pasting the  #
-# format of the pscustomobjects in the array                      #
+# Uses a custom function to import a registry key array from      #
+# a csv file, then create/set the key values as necessary         #
 #                                                                 #
 ###################################################################
 
@@ -30,92 +28,20 @@ If(!(test-path $logfile))
     New-Item $logfile
     }
 
-
-#Build registry keys array if Windows 10
+#Define the CSV file to pass to the function if Windows version is 10
 if ($winver -like '*Windows 10*')
-{
-$regkeys = @(
-[pscustomobject]@{
-    Key = "HKCU:\Software\Microsoft\Windows\ContentDeliveryManager"
-    Name = "SubscribedContent-338388Enabled"
-    Type = "DWORD"
-    Value = "0"
-    }
-    )
-}
-
-#Build registry keys array if Windows 11
-if ($winver -like '*Windows 11*')
-{
-$regkeys = @(
-[pscustomobject]@{
-    Key = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
-    Name = "TaskbarAl"
-    Type = "DWORD"
-    Value = "0"
-    }
-[pscustomobject]@{
-    Key = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
-    Name = "TaskbarMn"
-    Type = "DWORD"
-    Value = "0"
-    }
-[pscustomobject]@{
-    Key = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
-    Name = "TaskbarDa"
-    Type = "DWORD"
-    Value = "0"
-    }
-[pscustomobject]@{
-    Key = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
-    Name = "ShowTaskViewButton"
-    Type = "DWORD"
-    Value = "0"
-    }
-[pscustomobject]@{
-    Key = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
-    Name = "Start_IrisRecommendations"
-    Type = "DWORD"
-    Value = "0"
-    }
-    )
-}
-
-#Run commands against each registry key in the array
-foreach ($reg in $regkeys)
     {
-    #If the reg key does not already exist, create it
-    if(!(Get-ItemProperty -Path $reg.key -Name $reg.name))
-        {
-        New-ItemProperty -Path $reg.key -Name $reg.name -Value $reg.value  -PropertyType $reg.type
-        }
-    #If the existing reg key value does not match the array, update it
-    if(((Get-ItemProperty -Path $reg.key -Name $reg.name).($reg.name)) -ne $reg.value)
-        {
-        Set-ItemProperty -Path $reg.key -Name $reg.name -Value $reg.value
-        }
+    $csvfile = "$PSScriptRoot\win10regkeys.csv"
     }
- 
-#Check registry entries were correctly created and set, and write to log file
-foreach ($reg in $regkeys)
-        {
-        $regcheck = ((Get-ItemProperty -Path $reg.key -Name $reg.name).($reg.name))
-            #If reg entry is present and matches required value
-            if(($regcheck -ne $null) -and ($regcheck -eq $reg.value))
-            {
-            $datetime = (Get-Date)
-            Add-Content $logfile "$datetime - $reg present and set to correct value"
-            }
-            #If reg entry is present but does NOT match required value
-            if(($regcheck -ne $null) -and ($regcheck -ne $reg.value))
-            {
-            $datetime = (Get-Date)
-            Add-Content $logfile "$datetime - $reg present but set to incorrect value"
-            }
-            #If reg entry is not present
-            if($regcheck -eq $null)
-            {
-            $datetime = (Get-Date)
-            Add-Content $logfile "$datetime - $reg not present"
-            }
-        }  
+#Define the CSV file to pass to the function if Windows version is 11
+if ($winver -like '*Windows 11*')
+    {
+    $csvfile = "$PSScriptRoot\win11regkeys.csv"
+    }
+
+#Import the registry editing function and pass the defined csv and log files to it
+If(!(Get-Module -Name Set-Regkeys))
+    {
+    Import-Module "$PSScriptRoot\Set-Regkeys.psm1"
+    }
+Set-Regkeys -CsvImport $csvfile -LogResults $logfile
