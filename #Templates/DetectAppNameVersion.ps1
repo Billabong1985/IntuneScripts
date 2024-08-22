43f53fedb134a631c14dc1ea1f07129d7b3ed944
Billabong1985 <124009245+Billabong1985@users.noreply.github.com>
2024-08-22 12:38:01 +0100

    Update DetectAppNameVersion.ps1

--- #Templates/DetectAppNameVersion.ps1
+++ #Templates/DetectAppNameVersion.ps1
@@ -3,11 +3,11 @@ $AppName = "*App*Name*"
 #Define the package version number
 [version]$PackageVersion = "1.0.0"
 
-#Get app details from registry, add additional Where-Object qualifiers if more than one entry matches the display name
+#Get app details from registry, add additional Where-Object qualifiers if more than one app matches the display name variable
 $AppReg = Get-ChildItem -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall, HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall
 $AppNameReg = $AppReg | Get-ItemProperty | Where-Object {$_.DisplayName -like $AppName }
 
-#If multiple matching entries are still found (i.e. multiple versions or components of the app installed), select the one with the highest version number
+#If multiple matching entries are still found (i.e. multiple versions or components of the required app installed), select the one with the highest version number
 if(($AppNameReg).count -gt 1)
     {
     $AppNameReg = ($AppNameReg | Sort-Object -Descending {($AppNameReg).DisplayVersion})  | Select-Object -First 1
