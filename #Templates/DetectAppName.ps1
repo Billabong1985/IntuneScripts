a1f61ec3255d9b4e7a6b3a55290067f2fe9c0f85
Billabong1985 <124009245+Billabong1985@users.noreply.github.com>
2024-08-22 12:38:28 +0100

    Update DetectAppName.ps1

--- #Templates/DetectAppName.ps1
+++ #Templates/DetectAppName.ps1
@@ -1,7 +1,7 @@
 #Define the app display name, use wildcards to catch any minor changes to name between versions
 $AppName = "*App*Name*"
 
-#Get app details from registry, add additional Where-Object qualifiers if more than one entry matches the display name
+#Get app details from registry, add additional Where-Object qualifiers if more than one app matches the display name
 $AppReg = Get-ChildItem -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall, HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall
 $AppNameReg = $AppReg | Get-ItemProperty | Where-Object {$_.DisplayName -like $AppName}
 
