85622bdb3f92dbf650b9d3bda93568c041ce8e50
Billabong1985 <124009245+Billabong1985@users.noreply.github.com>
2024-08-23 23:10:56 +0100

    Update AppNameUninstall.ps1

--- #Templates/AppNameUninstall.ps1
+++ #Templates/AppNameUninstall.ps1
@@ -1,41 +1,36 @@
-#Define the app properties to filter from registry, leave the string blank for any unused properties and use partial wildcards to catch any minor variations on others
-#Note that the format for String values MUST be '"*String*"' to pass the "" characters through to the final filter string
-#The format for Property and Operator values should be in the "Value" format
+#Define the app properties to filter from registry use partial wildcards to catch any minor variations on display names
+#Unused properties should be set to '' or $null to exclude them from the filter string, or delete the pscustomobject line
 #In most cases the DisplayName -like property will be sufficient, additional properties can be used for apps that can't be singled out with that alone
 $RegFilters = @(
-    [pscustomobject]@{ Property = "DisplayName"; Operator = "-Like"; String = '"*App*Name*"' }
-    [pscustomobject]@{ Property = "DisplayName"; Operator = "-NotLike"; String = '' }
-    [pscustomobject]@{ Property = "Publisher"; Operator = "-Like"; String = '"*Publisher*"' }
-    [pscustomobject]@{ Property = "InstallLocation"; Operator = "-Like"; String = '' }
+    [pscustomobject]@{ Property = "DisplayName"; Operator = "Like"; String = "*App*Name*" }
+    [pscustomobject]@{ Property = "DisplayName"; Operator = "NotLike"; String = "" }
+    [pscustomobject]@{ Property = "Publisher"; Operator = "Like"; String = "*Publisher*" }
+    [pscustomobject]@{ Property = "InstallLocation"; Operator = "Like"; String = "" }
 )
 
-#Build an array of conditions for all properties with a string value
-$Filters = @()
-foreach($Property in $RegFilters)
-        {
-            if(($Property).String)
-            {
-            $Filters += "(" + (('$_.' + ($Property).Property) + " " + (($Property).Operator) + " " + (($Property).String)) + ")"
-            }
-        }
-#Combine the array into a single string
-$AllFilters = $Filters -join " -and "
+#Create a filter format template
+$FilterTemplate = '$_.{0} -{1} "{2}"'
+#Build a combined filter string using the format template, based on the $RegFilters variables with a String value
+#-Replace '(.+)', '($0)' encapsulates each individual filter in parentheses, which is not strictly necessary, but can help readability
+$AllFilters = $RegFilters.Where({ $_.String }).foreach({ $FilterTemplate -f $_.Property, $_.Operator, $_.String }) -Replace '(.+)', '($0)' -Join ' -and '
+#Convert the string to a scriptblock
+$AllFiltersScript = [scriptblock]::Create($AllFilters)
 
 #Get app details from registry
 $AppReg = Get-ChildItem -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall, HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall
-$AppNameReg = $AppReg | Get-ItemProperty | Where-Object { Invoke-Expression $AllFilters}
+$AppNameReg = @($AppReg | Get-ItemProperty | Where-Object -FilterScript $AllFiltersScript)
 
 #Use a foreach loop to uninstall all versions of the app if more than one result is returned
 foreach($App in $AppNameReg)
     { 
     #Set the variable to the quiet uninstall string if it exists, otherwise use standard uninstall string
-    if($null -ne ($AppNameReg).QuietUninstallString)
+    if($null -ne $AppNameReg.QuietUninstallString)
         {
-        $Uninstall = ($AppNameReg).QuietUninstallString
+        $Uninstall = $AppNameReg.QuietUninstallString
         }
     else
         {
-        $Uninstall = ($AppNameReg).UninstallString
+        $Uninstall = $AppNameReg.UninstallString
         }
 
     #Split the string to define arguments then execute
