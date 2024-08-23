bf63b8ee6bdf7fd14ed0df749da899c2e0aff5cd
Billabong1985 <124009245+Billabong1985@users.noreply.github.com>
2024-08-23 23:03:20 +0100

    Update DetectAppName.ps1

--- #Templates/DetectAppName.ps1
+++ #Templates/DetectAppName.ps1
@@ -1,40 +1,34 @@
-#Define the app properties to filter from registry, leave the string value blank for any unused properties or remove the respective pscustom object
-#Partial wildcards can be used for string values to catch any minor variations between versions
-#Note that the format for String values MUST be '"*String*"' to pass the "" characters through to the final filter string
-#The format for Property and Operator values should be in the "Value" format
+#Define the app properties to filter from registry use partial wildcards to catch any minor variations on display names
+#Unused properties should be set to '' or $null to exclude them from the filter string, or delete the pscustomobject line
 #In most cases the DisplayName -like property will be sufficient, additional properties can be used for apps that can't be singled out with that alone
 $RegFilters = @(
-    [pscustomobject]@{ Property = "DisplayName"; Operator = "-Like"; String = '"*App*Name*"' }
-    [pscustomobject]@{ Property = "DisplayName"; Operator = "-NotLike"; String = '' }
-    [pscustomobject]@{ Property = "Publisher"; Operator = "-Like"; String = '"*Publisher*"' }
-    [pscustomobject]@{ Property = "InstallLocation"; Operator = "-Like"; String = '' }
+    [pscustomobject]@{ Property = "DisplayName"; Operator = "Like"; String = "*App*Name*" }
+    [pscustomobject]@{ Property = "DisplayName"; Operator = "NotLike"; String = "" }
+    [pscustomobject]@{ Property = "Publisher"; Operator = "Like"; String = "*Publisher*" }
+    [pscustomobject]@{ Property = "InstallLocation"; Operator = "Like"; String = "" }
 )
 
-#Build an array of conditions for all properties with a string value
-$Filters = @()
-foreach($Property in $RegFilters)
-        {
-            if(($Property).String)
-            {
-            $Filters += "(" + (('$_.' + ($Property).Property) + " " + (($Property).Operator) + " " + (($Property).String)) + ")"
-            }
-        }
-#Combine the array into a single string
-$AllFilters = $Filters -join " -and "
+#Create a filter format template
+$FilterTemplate = '$_.{0} -{1} "{2}"'
+#Build a combined filter string using the format template, based on the $RegFilters variables with a String value
+#-Replace '(.+)', '($0)' encapsulates each individual filter in parentheses, which is not strictly necessary, but can help readability
+$AllFilters = $RegFilters.Where({ $_.String }).foreach({ $FilterTemplate -f $_.Property, $_.Operator, $_.String }) -Replace '(.+)', '($0)' -Join ' -and '
+#Convert the string to a scriptblock
+$AllFiltersScript = [scriptblock]::Create($AllFilters)
 
 #Get app details from registry
 $AppReg = Get-ChildItem -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall, HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall
-$AppNameReg = $AppReg | Get-ItemProperty | Where-Object { Invoke-Expression $AllFilters}
+$AppNameReg = @($AppReg | Get-ItemProperty | Where-Object -FilterScript $AllFiltersScript)
 
 #If 1 app is returned, write output and exit 0
-if(($AppNameReg).count -eq 1)
+if($AppNameReg.count -eq 1)
     {
     write-host Installed
     Exit 0
     }
 
 #If more than 1 app is returned from registry, write a log file with the details for review and exit 1
-if(($AppNameReg).count -gt 1)
+if($AppNameReg.count -gt 1)
     {
     $LogFolder = "C:\Software\AppDetection"
     $FileName = (($RegFilters).String[0]) -Replace '["*]',''
