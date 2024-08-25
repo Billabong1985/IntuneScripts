1d89e1992e737c3c92835224c6902e63d5720017
Billabong1985 <124009245+Billabong1985@users.noreply.github.com>
2024-08-25 20:36:00 +0100

    Add files via upload

---
+++ NodeJSUpdate/DetectNodejsVersion.ps1
@@ -0,0 +1,66 @@
+﻿#Define the package version numbers
+$PackageVersions = @("18.20.2", "20.12.2")
+$PackageVersions = $PackageVersions.ForEach{ [version]$_ }
+#Define the app properties to filter from registry use partial wildcards to catch any minor variations on display names
+#Unused properties should be set to "" or $null to exclude them from the filter string, or delete the pscustomobject line
+#In most cases the DisplayName -like property will be sufficient, additional properties can be used for apps that can't be singled out with that alone
+$RegFilters = @(
+    [pscustomobject]@{ Property = "DisplayName"; Operator = "Like"; String = "*Node*JS*" }
+    [pscustomobject]@{ Property = "DisplayName"; Operator = "NotLike"; String = "" }
+    [pscustomobject]@{ Property = "Publisher"; Operator = "Like"; String = "" }
+    [pscustomobject]@{ Property = "InstallLocation"; Operator = "Eq"; String = "" }
+)
+
+#Create a filter format template
+$FilterTemplate = '$_.{0} -{1} "{2}"'
+#Build a combined filter string using the format template, based on the $RegFilters variables with a String value
+#-Replace '(.+)', '($0)' encapsulates each individual filter in parentheses, which is not strictly necessary, but can help readability
+$AllFilters = $RegFilters.Where({ $_.String }).foreach({ $FilterTemplate -f $_.Property, $_.Operator, $_.String }) -Replace '(.+)', '($0)' -Join ' -and '
+#Convert the string to a scriptblock
+$AllFiltersScript = [scriptblock]::Create($AllFilters)
+
+#Get app details from registry
+$AppReg = Get-ChildItem -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall, HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall
+$AppNameReg = @($AppReg | Get-ItemProperty | Where-Object -FilterScript $AllFiltersScript)
+
+#If no more apps are returned than the number of specified versions, compare version numbers with the corresponding package versions
+if (($AppNameReg.count -gt 0) -and ($AppNameReg.count -le $PackageVersions.count)) {
+    foreach ($App in $AppNameReg) {
+        #Create an array to record up ot date versions
+        $InstallComplete = @()
+        #Define the currently installed version number
+        [version]$CurrentVersion = $App.DisplayVersion
+        $CurrentVersionMajor = $CurrentVersion.Major
+        #Find the version in the $PackageVersions array that has matching Major version
+        $MatchingAppVersion = $PackageVersions | Where-Object { $_.Major -eq $CurrentVersionMajor }
+        if (($null -ne $MatchingVersion) -and ($CurrentVersion -ge $MatchingAppVersion)) {
+            $InstallComplete += [PSCustomObject]@{ Version = $MatchingAppVersion; Installed = "True" }
+            }
+        if (($null -ne $MatchingVersion) -and ($CurrentVersion -lt $MatchingAppVersion)) {
+            $InstallComplete += [PSCustomObject]@{ Version = $MatchingAppVersion; Installed = "False" }
+            }
+        }
+    }
+
+#If at least 1  install has been detected and the version number is greater than or equal to the specified package version(s), write to host and exit 0
+If(($null -ne $InstallComplete)-and ("False" -notin $InstallComplete.Installed)) {
+    Write-Host Installed
+    Exit 0
+}
+    
+#If more than apps are returned from registry than specified versions, write a log file with the details for review and exit 1
+if ($AppNameReg.count -gt $PackageVersions.count) {
+    $LogFolder = "C:\Software\AppVersionDetection"
+    if (!(Test-Path $LogFolder)) {
+        New-Item -ItemType Directory -Force -Path $logfolder
+    }
+    $FileName = (($RegFilters).String[0]) -Replace "[*]", ""
+    $LogFile = "$LogFolder\$FileName.log"
+    $DateTime = (Get-Date)
+    Clear-Content $LogFile -ErrorAction Ignore
+    Add-Content $LogFile $DateTime
+    Add-Content $LogFile "Too many apps filtered based on supplied criteria, version number(s) cannot be confirmed"
+    Add-Content $LogFile "See list of returned registry keys below and refine filters to single out correct app(s)"
+    $AppNameReg | ForEach-Object { $_ | Out-String } | Add-Content -Path $LogFile
+    Exit 1
+}
---
+++ NodeJSUpdate/InstallNodejs.ps1
@@ -0,0 +1,46 @@
+﻿#Define the path the MSI files are stored in under the scriptroot. This can be root level but a subfolder containing only install files is preferable
+$FilePath = "$PSScriptRoot\MSI"
+
+#Define the app properties to filter from registry. Use partial wildcards to catch any minor variations on display names
+$RegFilters = @(
+    [pscustomobject]@{ Property = "DisplayName"; Operator = "Like"; String = "*node.js*" }
+    [pscustomobject]@{ Property = "DisplayName"; Operator = "NotLike"; String = "" }
+    [pscustomobject]@{ Property = "Publisher"; Operator = "Like"; String = "" }
+    [pscustomobject]@{ Property = "InstallLocation"; Operator = "Eq"; String = "" }
+)
+
+#Pull verion numbers from MSI files, create PSCustomObjects array to hold corresponding filename, display version and [version]
+$FileNames = @((Get-ChildItem -Path $FilePath -Filter *.* -Recurse | Where-Object {($_.Extension -eq ".msi")}).Name)
+$DisplayVersions = @()
+$PackageObjects = @()
+foreach($File in $FileNames) {
+    $FileVersion = (Get-MSIProperty productversion -Path $FilePath\$File).value
+    $DisplayVersions += $FileVersion
+    $PackageObjects += [PSCustomObject]@{ FilePath = "$FilePath\$File"; DisplayVersion = $FileVersion; PackageVersion = [version]$FileVersion }  
+    }
+
+#Create a filter format template
+$FilterTemplate = '$_.{0} -{1} "{2}"'
+#Build a combined filter string using the format template, based on the $RegFilters variables with a String value
+#-Replace '(.+)', '($0)' encapsulates each individual filter in parentheses, which is not strictly necessary, but can help readability
+$AllFilters = $RegFilters.Where({ $_.String }).foreach({ $FilterTemplate -f $_.Property, $_.Operator, $_.String }) -Replace '(.+)', '($0)' -Join ' -and '
+#Convert the string to a scriptblock
+$AllFiltersScript = [scriptblock]::Create($AllFilters)
+
+#Get app details from registry
+$AppReg = Get-ChildItem -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall, HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall
+$AppNameReg = @($AppReg | Get-ItemProperty | Where-Object -FilterScript $AllFiltersScript)
+
+#Compare each result's version number with the package version held in the Package array
+foreach ($App in $AppNameReg) {
+    #Define the currently installed version number
+    [version]$CurrentVersion = $App.DisplayVersion
+    #Find an object in the Package array with a matching Major version number
+    $MatchingPackage = $PackageObjects | Where-Object { $_.PackageVersion.Major -eq $CurrentVersion.Major}
+
+    #If the current version is less than the corresponding Package version, install the package
+    if($CurrentVersion -lt $MatchingPackage.PackageVersion) {
+        $MSI = $MatchingPackage.FilePath
+        start-process msiexec.exe -Wait -ArgumentList "/i $MSI /qn"
+    }
+}
