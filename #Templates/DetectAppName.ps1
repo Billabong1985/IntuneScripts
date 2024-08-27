52910a7f5130123e82f043aa0f47cd01ee9a91e4
Billabong1985 <124009245+Billabong1985@users.noreply.github.com>
2024-08-27 16:41:43 +0100

    Update DetectAppName.ps1

--- #Templates/DetectAppName.ps1
+++ #Templates/DetectAppName.ps1
@@ -31,7 +31,7 @@ function Get-AppReg {
 }
 
 #Define the app registry entry by calling the function. -AppNameNotLike is set up as an array and can accept multiple strings
-$AppNameReg = Get-AppReg -AppNameLike "*App*Name*" -AppNameNotLike @("*Exclude*","*Exclude2*") -PublisherLike "*Publisher*"
+$AppNameReg = @(Get-AppReg -AppNameLike "*App*Name*" -AppNameNotLike @("*Exclude*","*Exclude2*") -PublisherLike "*Publisher*")
 
 #If 1 app is returned, write output and exit 0
 if ($AppNameReg.count -eq 1) {
