f34c4e78da865e0def5aaab1075cb35efbcf2d71
Billabong1985 <124009245+Billabong1985@users.noreply.github.com>
2024-08-27 13:38:31 +0100

    Update AppNameUninstall.ps1

--- #Templates/AppNameUninstall.ps1
+++ #Templates/AppNameUninstall.ps1
@@ -1,20 +1,20 @@
 #Create the function
 function Get-AppReg {
-    #Define the parameters
+    #Define the Parameters
     param(
         [Parameter(Mandatory = $true)][string]$AppNameLike,
-        [Parameter(Mandatory = $false)][string]$AppNameNotLike,
         [Parameter(Mandatory = $false)][string]$PublisherLike,
-        [Parameter(Mandatory = $false)][string]$InstallPathEq
+        [Parameter(Mandatory = $false)][string[]]$AppNameNotLike
     )
 
     #Create an array of objects for the registry search
     $RegFilters = @(
-        [pscustomobject]@{ Property = "DisplayName"; Operator = "Like"; String = "$AppNameLike" }
-        [pscustomobject]@{ Property = "DisplayName"; Operator = "NotLike"; String = "$AppNameNotLike" }
-        [pscustomobject]@{ Property = "Publisher"; Operator = "Like"; String = "$PublisherLike" }
-        [pscustomobject]@{ Property = "InstallLocation"; Operator = "Eq"; String = "$InstallPathEq" }
+        [pscustomobject]@{ Property = "DisplayName"; Operator = "Like"; String = $AppNameLike }
+        [pscustomobject]@{ Property = "Publisher"; Operator = "Like"; String = $PublisherLike }
     )
+    foreach($String in $AppNameNotLike) {
+        $RegFilters += [pscustomobject]@{ Property = "DisplayName"; Operator = "NotLike"; String = "$String" }
+    }
 
     #Create a filter format template
     $FilterTemplate = '$_.{0} -{1} "{2}"'
@@ -30,32 +30,27 @@ function Get-AppReg {
     Write-Output $AppReg
 }
 
-#Define the app registry entry by calling the function
-$AppNameReg = Get-AppReg -AppNameLike "*App*Name*" -PublisherLike "*Publisher*"
+#Define the app registry entry by calling the function. -AppNameNotLike is set up as an array and can accept multiple strings
+$AppNameReg = Get-AppReg -AppNameLike "*App*Name*" -AppNameNotLike @("*Exclude*","*Exclude2*") -PublisherLike "*Publisher*"
 
 #Use a foreach loop to uninstall all versions of the app if more than one result is returned
-foreach($App in $AppNameReg)
-    { 
+foreach ($App in $AppNameReg) { 
     #Set the variable to the quiet uninstall string if it exists, otherwise use standard uninstall string
-    if($null -ne $App.QuietUninstallString)
-        {
+    if ($null -ne $App.QuietUninstallString) {
         $Uninstall = $App.QuietUninstallString
-        }
-    else
-        {
+    }
+    else {
         $Uninstall = $App.UninstallString
-        }
+    }
 
     #Split the string to define arguments then execute
     $MSI = "MsiExec.exe"
-    if($Uninstall -match $MSI)
-        {
+    if ($Uninstall -match $MSI) {
         $Identifier = $Uninstall  | Select-String -Pattern "\{[A-F0-9-]+\}" -AllMatches | ForEach-Object { $_.Matches.Value }
         Start-Process $MSI -Wait -ArgumentList "/x $Identifier /qn"
-        }
-    else 
-        {
+    }
+    else {
         $SplitString = ($Uninstall -split '"').Trim() | Where-Object { $_.Trim() -ne "" }
         Start-Process $SplitString[0] -Wait -ArgumentList $SplitString[1]
-        }
     }
+}
