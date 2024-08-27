b05e4b8e4a41afbc14d3acd63d254e79196c04f9
Billabong1985 <124009245+Billabong1985@users.noreply.github.com>
2024-08-27 10:34:31 +0100

    Update Set-Regkeys.psm1

--- AdobeReader/Set-Regkeys.psm1
+++ AdobeReader/Set-Regkeys.psm1
@@ -1,58 +1,55 @@
-function Set-Regkeys
-{
-param(
-    [Parameter(Mandatory=$true)]
-    [string]$CsvImport,
-    [Parameter(Mandatory=$false)]
-    [string]$LogResults
+function Set-RegKeys {
+    #Define the parameters
+    param(
+        [Parameter(Mandatory = $true)]
+        [string]$CsvImport,
+        [Parameter(Mandatory = $false)]
+        [string]$LogResults
     )
 
-#Create an array of reg keys from an imported CSV file
-$regkeys = Import-Csv $CsvImport
+    #Create an array of reg keys from an imported CSV file
+    $RegKeys = Import-Csv $CsvImport
 
-#Execute commands on each key imported
-foreach ($reg in $regkeys)
-    {
-    #If the reg key does not already exist, create it
-    if(!(Get-Item -Path $reg.key -ErrorAction SilentlyContinue))
-        {
-        New-Item -Path $reg.key
+    #Execute commands on each key imported
+    foreach ($Reg in $RegKeys) {
+        #If the reg key does not already exist, create it
+        if (!(Get-Item -Path $Reg.Key -ErrorAction SilentlyContinue)) {
+            New-Item -Path $Reg.Key
         }
-    #If the reg value does not already exist, create it
-    if(!(Get-ItemProperty -Path $reg.key -Name $reg.name -ErrorAction SilentlyContinue))
-        {
-        New-ItemProperty -Path $reg.key -Name $reg.name -Value $reg.value  -PropertyType $reg.type
+        #If the reg value does not already exist, create it
+        if (!(Get-ItemProperty -Path $Reg.Key -Name $Reg.Name -ErrorAction SilentlyContinue)) {
+            New-ItemProperty -Path $Reg.Key -Name $Reg.Name -Value $Reg.Value  -PropertyType $Reg.Type
         }
-    #If the existing reg value does not match the array, update it
-    if(((Get-ItemProperty -Path $reg.key -Name $reg.name).($reg.name)) -ne $reg.value)
-        {
-        Set-ItemProperty -Path $reg.key -Name $reg.name -Value $reg.value
+        #If the existing reg value does not match the array, update it
+        if (((Get-ItemProperty -Path $Reg.Key -Name $Reg.Name).($Reg.Name)) -ne $Reg.Value) {
+            Set-ItemProperty -Path $Reg.Key -Name $Reg.Name -Value $Reg.Value
         }
     }
 
-#If a log file parameter was passed, check values and log results
-If($PSBoundParameters.ContainsKey("LogResults"))
-    {
-    foreach ($reg in $regkeys)
-        {
-        $regcheck = ((Get-ItemProperty -Path $reg.key -Name $reg.name).($reg.name))
-        #If reg entry is present and matches required value
-        if(($null -ne $regcheck) -and ($regcheck -eq $reg.value))
-            {
-            $datetime = (Get-Date)
-            Add-Content $LogResults "$datetime - $reg present and set to correct value"
+    #If a log file parameter was passed, check values and log results
+    If ($PSBoundParameters.ContainsKey("LogResults")) {
+        #Create the log folder if it does not already exist
+        $LogResultsPath = Split-Path $LogResults -Parent
+        If (!(Test-Path $LogResultsPath)) {
+            New-Item -ItemType Directory -Force -Path $LogResultsPath
+        }
+        #Check the keys and write to the log file
+        foreach ($Reg in $Regkeys) {
+            $RegCheck = ((Get-ItemProperty -Path $Reg.Key -Name $Reg.Name).($Reg.Name))
+            #If reg entry is present and matches required value
+            if (($null -ne $RegCheck) -and ($RegCheck -eq $Reg.Value)) {
+                $DateTime = (Get-Date)
+                Add-Content $LogResults "$DateTime - $Reg present and set to correct value"
             }
-        #If reg entry is present but does NOT match required value
-        if(($null -ne $regcheck) -and ($regcheck -ne $reg.value))
-            {
-            $datetime = (Get-Date)
-            Add-Content $LogResults "$datetime - $reg present but set to incorrect value"
+            #If reg entry is present but does NOT match required value
+            if (($null -ne $RegCheck) -and ($RegCheck -ne $Reg.Value)) {
+                $DateTime = (Get-Date)
+                Add-Content $LogResults "$DateTime - $Reg present but set to incorrect value"
             }
-        #If reg entry is not present
-        if($null -eq $regcheck)
-            {
-            $datetime = (Get-Date)
-            Add-Content $LogResults "$datetime - $reg not present"
+            #If reg entry is not present
+            if ($null -eq $RegCheck) {
+                $DateTime = (Get-Date)
+                Add-Content $LogResults "$DateTime - $Reg not present"
             }
         }
     }
