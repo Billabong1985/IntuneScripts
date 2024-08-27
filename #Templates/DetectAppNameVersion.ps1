d642d25c288586469e89f02e642ddc13c3ba66a2
Billabong1985 <124009245+Billabong1985@users.noreply.github.com>
2024-08-27 10:26:49 +0100

    Update DetectAppNameVersion.ps1

--- #Templates/DetectAppNameVersion.ps1
+++ #Templates/DetectAppNameVersion.ps1
@@ -1,27 +1,39 @@
-#Define the package version number
+#Create the function
+function Get-AppReg {
+    #Define the Parameters
+    param(
+        [Parameter(Mandatory = $true)][string]$AppNameLike,
+        [Parameter(Mandatory = $false)][string]$AppNameNotLike,
+        [Parameter(Mandatory = $false)][string]$PublisherLike,
+        [Parameter(Mandatory = $false)][string]$InstallPathEq
+    )
+
+    #Create an array of objects for the registry search
+    $RegFilters = @(
+        [pscustomobject]@{ Property = "DisplayName"; Operator = "Like"; String = "$AppNameLike" }
+        [pscustomobject]@{ Property = "DisplayName"; Operator = "NotLike"; String = "$AppNameNotLike" }
+        [pscustomobject]@{ Property = "Publisher"; Operator = "Like"; String = "$PublisherLike" }
+        [pscustomobject]@{ Property = "InstallLocation"; Operator = "Eq"; String = "$InstallPathEq" }
+    )
+
+    #Create a filter format template
+    $FilterTemplate = '$_.{0} -{1} "{2}"'
+    #Build a combined filter string using the format template, based on the $RegFilters variables with a String value
+    #-Replace '(.+)', '($0)' encapsulates each individual filter in parentheses, which is not strictly necessary, but can help readability
+    $AllFilters = $RegFilters.Where({ $_.String }).foreach({ $FilterTemplate -f $_.Property, $_.Operator, $_.String }) -Replace '(.+)', '($0)' -Join ' -and '
+    #Convert the string to a scriptblock
+    $AllFiltersScript = [scriptblock]::Create($AllFilters)
+
+    #Get app details from registry and write output
+    $AllAppsReg = Get-ChildItem -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall, HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall
+    $AppReg = @($AllAppsReg | Get-ItemProperty | Where-Object -FilterScript $AllFiltersScript)
+    Write-Output $AppReg
+}
+
+#Define the app registry entry by calling the function
+$AppNameReg = Get-AppReg -AppNameLike "*App*Name*" -PublisherLike "*Publisher*"
+#Define the package version
 [version]$PackageVersion = "1.0.0"
-
-#Define the app properties to filter from registry use partial wildcards to catch any minor variations on display names
-#Unused properties should be set to "" or $null to exclude them from the filter string, or delete the pscustomobject line
-#In most cases the DisplayName -like property will be sufficient, additional properties can be used for apps that can't be singled out with that alone
-$RegFilters = @(
-    [pscustomobject]@{ Property = "DisplayName"; Operator = "Like"; String = "*App*Name*" }
-    [pscustomobject]@{ Property = "DisplayName"; Operator = "NotLike"; String = "" }
-    [pscustomobject]@{ Property = "Publisher"; Operator = "Like"; String = "*Publisher*" }
-    [pscustomobject]@{ Property = "InstallLocation"; Operator = "Eq"; String = "" }
-)
-
-#Create a filter format template
-$FilterTemplate = '$_.{0} -{1} "{2}"'
-#Build a combined filter string using the format template, based on the $RegFilters variables with a String value
-#-Replace '(.+)', '($0)' encapsulates each individual filter in parentheses, which is not strictly necessary, but can help readability
-$AllFilters = $RegFilters.Where({ $_.String }).foreach({ $FilterTemplate -f $_.Property, $_.Operator, $_.String }) -Replace '(.+)', '($0)' -Join ' -and '
-#Convert the string to a scriptblock
-$AllFiltersScript = [scriptblock]::Create($AllFilters)
-
-#Get app details from registry
-$AppReg = Get-ChildItem -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall, HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall
-$AppNameReg = @($AppReg | Get-ItemProperty | Where-Object -FilterScript $AllFiltersScript)
 
 #If 1 app is returned, compare it's version number with the package version
 if ($AppNameReg.count -eq 1) {
@@ -40,7 +52,7 @@ if ($AppNameReg.count -gt 1) {
     if (!(Test-Path $LogFolder)) {
         New-Item -ItemType Directory -Force -Path $logfolder
     }
-    $FileName = (($RegFilters).String[0]) -Replace "[*]",""
+    $FileName = (($RegFilters).String[0]) -Replace "[*]", ""
     $LogFile = "$LogFolder\$FileName.log"
     $DateTime = (Get-Date)
     Clear-Content $LogFile -ErrorAction Ignore
