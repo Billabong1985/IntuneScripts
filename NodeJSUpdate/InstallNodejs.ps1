a4bc6b5bdd3d5daf49a662f3606dfeaf18b669c5
Billabong1985 <124009245+Billabong1985@users.noreply.github.com>
2024-08-27 10:45:13 +0100

    Update InstallNodejs.ps1

--- NodeJSUpdate/InstallNodejs.ps1
+++ NodeJSUpdate/InstallNodejs.ps1
@@ -1,17 +1,44 @@
+#Create the function
+function Get-AppReg {
+    #Define the parameters
+    param(
+        [Parameter(Mandatory = $true)][string]$AppNameLike,
+        [Parameter(Mandatory = $false)][string]$AppNameNotLike,
+        [Parameter(Mandatory = $false)][string]$PublisherLike,
+        [Parameter(Mandatory = $false)][string]$InstallPathEq
+    )
+
+    #Create an array of objects for the registry search
+    $RegFilters = @(
+        [pscustomobject]@{ Property = "DisplayName"; Operator = "Like"; String = "$AppNameLike" }
+        [pscustomobject]@{ Property = "DisplayName"; Operator = "NotLike"; String = "$AppNameNotLike" }
+        [pscustomobject]@{ Property = "Publisher"; Operator = "Like"; String = "$PublisherLike" }
+        [pscustomobject]@{ Property = "InstallLocation"; Operator = "Eq"; String = "$InstallPathEq" }
+    )
+
+    #Create a filter format template
+    $FilterTemplate = '$_.{0} -{1} "{2}"'
+    #Build a combined filter string using the format template, based on the $RegFilters variables with a String value
+    #-Replace '(.+)', '($0)' encapsulates each individual filter in parentheses, which is not strictly necessary, but can help readability
+    $AllFilters = $RegFilters.Where({ $_.String }).foreach({ $FilterTemplate -f $_.Property, $_.Operator, $_.String }) -Replace '(.+)', '($0)' -Join ' -and '
+    #Convert the string to a scriptblock
+    $AllFiltersScript = [scriptblock]::Create($AllFilters)
+
+    #Get app details from registry and write output
+    $AllAppsReg = Get-ChildItem -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall, HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall
+    $AppReg = @($AllAppsReg | Get-ItemProperty | Where-Object -FilterScript $AllFiltersScript)
+    Write-Output $AppReg
+}
+
+#Define the app registry entry by calling the function
+$AppNameReg = Get-AppReg -AppNameLike "*node.js*"
+
 #Define the path the MSI files are stored in under the scriptroot. This can be root level but a subfolder containing only install files is preferable
 $FilePath = "$PSScriptRoot\MSI"
 
 #Set minimum safe version(s)
 $SafeVersions = @("18.18.2", "20.8.1")
 $SafeVersions = $SafeVersions.ForEach{ [version]$_ }
-
-#Define the app properties to filter from registry. Use partial wildcards to catch any minor variations on display names
-$RegFilters = @(
-    [pscustomobject]@{ Property = "DisplayName"; Operator = "Like"; String = "*node.js*" }
-    [pscustomobject]@{ Property = "DisplayName"; Operator = "NotLike"; String = "" }
-    [pscustomobject]@{ Property = "Publisher"; Operator = "Like"; String = "" }
-    [pscustomobject]@{ Property = "InstallLocation"; Operator = "Eq"; String = "" }
-)
 
 #Pull verion numbers from MSI files, create PSCustomObjects array to hold corresponding filename, display version and [version]
 $FileNames = @((Get-ChildItem -Path $FilePath -Filter *.* -Recurse | Where-Object { ($_.Extension -eq ".msi") }).Name)
@@ -22,18 +49,6 @@ foreach ($File in $FileNames) {
     $DisplayVersions += $FileVersion
     $PackageObjects += [PSCustomObject]@{ FilePath = "$FilePath\$File"; DisplayVersion = $FileVersion; PackageVersion = [version]$FileVersion }  
 }
-
-#Create a filter format template
-$FilterTemplate = '$_.{0} -{1} "{2}"'
-#Build a combined filter string using the format template, based on the $RegFilters variables with a String value
-#-Replace '(.+)', '($0)' encapsulates each individual filter in parentheses, which is not strictly necessary, but can help readability
-$AllFilters = $RegFilters.Where({ $_.String }).foreach({ $FilterTemplate -f $_.Property, $_.Operator, $_.String }) -Replace '(.+)', '($0)' -Join ' -and '
-#Convert the string to a scriptblock
-$AllFiltersScript = [scriptblock]::Create($AllFilters)
-
-#Get app details from registry
-$AppReg = Get-ChildItem -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall, HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall
-$AppNameReg = @($AppReg | Get-ItemProperty | Where-Object -FilterScript $AllFiltersScript)
 
 #Compare each result's version number with the package version held in the Package array
 foreach ($App in $AppNameReg) {
