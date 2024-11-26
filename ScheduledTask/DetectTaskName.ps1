daeb246f21e46e7c73926884b1ec4fa3e90242d7
Billabong1985 <124009245+Billabong1985@users.noreply.github.com>
2024-11-26 16:15:16 +0000

    Create DetectTaskName.ps1

---
+++ ScheduledTask/DetectTaskName.ps1
@@ -0,0 +1,16 @@
+#Define expected versions
+$TaskVersion = "V1.0"
+$ScriptVersion = "V1.0"
+
+#Define variables
+$TaskName = "TaskName"
+$ScriptFolder = "C:\Software\TaskName"
+$ScriptFile = "$ScriptFolder\TaskScript.ps1"
+
+#Get the description of the scheduled task, this should be the version number
+$TaskDescription = (Get-ScheduledTask -TaskName $TaskName -ErrorAction Ignore).Description
+
+if (($TaskDescription -eq $TaskVersion) -and (get-content $ScriptFile -ErrorAction Ignore | Select-String ($ScriptVersion))) {
+    Write-Host "Configured"
+    Exit 0
+}
