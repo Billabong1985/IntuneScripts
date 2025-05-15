2cd1201b0c205bdcbd18a78fb84eba5af34107a8
Billabong1985 <124009245+Billabong1985@users.noreply.github.com>
2025-05-15 15:27:36 +0100

    Update AcroRdrDCUninstall.ps1

--- AdobeReader/AcroRdrDCUninstall.ps1
+++ AdobeReader/AcroRdrDCUninstall.ps1
@@ -1,3 +1,20 @@
+#Variables here to be passed through to the script below the function
+#Define log folder
+$LogFolder = "C:\Software\AcrobatReader"
+if (!(Test-Path $LogFolder)) {
+    New-Item -ItemType Directory -Force -Path $LogFolder
+}
+$LogFile = "$LogFolder\AcroRdrUninstall.log"
+
+#Define the registry search strings, hash out any that are not needed
+$AppNameSearchString = '"*Acrobat*"'
+$AppNameSearchExcludeString = '@("*Refresh*Manager*", "*Customization*Wizard*")'
+$PublisherSearchString = '"*Adobe*"'
+
+#Define the command to pass to the function, remove any unused parts
+$GetAppRegCommand = "(Get-AppReg -AppNameLike $AppNameSearchString -AppNameNotLike $AppNameSearchExcludeString -PublisherLike $PublisherSearchString)"
+#End of variables to be changed
+
 #Create the function
 function Get-AppReg {
     #Define the Parameters
@@ -12,7 +29,7 @@ function Get-AppReg {
         [pscustomobject]@{ Property = "DisplayName"; Operator = "Like"; String = $AppNameLike }
         [pscustomobject]@{ Property = "Publisher"; Operator = "Like"; String = $PublisherLike }
     )
-    foreach($String in $AppNameNotLike) {
+    foreach ($String in $AppNameNotLike) {
         $RegFilters += [pscustomobject]@{ Property = "DisplayName"; Operator = "NotLike"; String = "$String" }
     }
 
@@ -30,32 +47,57 @@ function Get-AppReg {
     Write-Output $AppReg
 }
 
-#Define the app registry entry by calling the function
-$AppNameReg = @(Get-AppReg -AppNameLike "*Acrobat*" -PublisherLike "*Adobe*" -AppNameNotLike @("*Refresh*Manager*","*Customization*Wizard*"))
+#Start Logging
+Start-Transcript -Path $LogFile
 
-#Use a foreach loop to uninstall all versions of the app if more than one result is returned
-foreach($App in $AppNameReg)
-    { 
-    #Set the variable to the quiet uninstall string if it exists, otherwise use standard uninstall string
-    if($null -ne $App.QuietUninstallString)
-        {
-        $Uninstall = $App.QuietUninstallString
+#Define the app registry entry by calling the function. -AppNameNotLike is set up as an array and can accept multiple strings
+$AppNameReg = @(Invoke-Expression $GetAppRegCommand)
+
+#If no matching apps are found, write host and exit
+If (!($AppNameReg)) {
+    Write-Host "$(Get-Date): No matching apps found, script will now exit"
+    Exit 0
+    #Otherwise continue with the uninstall script
+}
+Else {
+    #Use a foreach loop to uninstall all versions of the app if more than one result is returned
+    foreach ($App in $AppNameReg) { 
+        Write-Host "$(Get-Date): Found $($App.DisplayName) version $($App.DisplayVersion)"
+
+        #Set the variable to the quiet uninstall string if it exists, otherwise use standard uninstall string
+        if ($null -ne $App.QuietUninstallString) {
+            $Uninstall = $App.QuietUninstallString
         }
-    else
-        {
-        $Uninstall = $App.UninstallString
+        else {
+            $Uninstall = $App.UninstallString
         }
 
-    #Split the string to define arguments then execute
-    $MSI = "MsiExec.exe"
-    if($Uninstall -match $MSI)
-        {
-        $Identifier = $Uninstall  | Select-String -Pattern "\{[A-F0-9-]+\}" -AllMatches | ForEach-Object { $_.Matches.Value }
-        Start-Process $MSI -Wait -ArgumentList "/x $Identifier /qn"
+        #Split the string to define arguments then execute
+        $MSI = "MsiExec.exe"
+        if ($Uninstall -match $MSI) {
+            $Identifier = $Uninstall  | Select-String -Pattern "\{[A-F0-9-]+\}" -AllMatches | ForEach-Object { $_.Matches.Value }
+            Write-Host "$(Get-Date): Uninstalling $($App.DisplayName) version $($App.DisplayVersion)..."
+            Start-Process $MSI -Wait -ArgumentList "/x $Identifier /qn"
         }
-    else 
-        {
-        $SplitString = ($Uninstall -split '"').Trim() | Where-Object { $_.Trim() -ne "" }
-        Start-Process $SplitString[0] -Wait -ArgumentList $SplitString[1]
+        else {
+            $SplitString = ($Uninstall -split '"').Trim() | Where-Object { $_.Trim() -ne "" }
+            Write-Host "$(Get-Date): Uninstalling $($App.DisplayName) version $($App.DisplayVersion)..."
+            Start-Process $SplitString[0] -Wait -ArgumentList $SplitString[1]
         }
     }
+    #Check registry again
+    $AppNameReg = @(Invoke-Expression $GetAppRegCommand)
+
+    #Write results
+    if (!($AppNameReg)) {
+        Write-Host "$(Get-Date): All detected apps have been successfully uninstalled"
+    }
+    else {
+        ForEach ($App in $AppNameReg) {
+            Write-Host "$(Get-Date): $($App.DisplayName) version $($App.DisplayVersion) still detected after uninstall attempt"
+        }
+    }
+}
+
+#Stop Logging
+Stop-Transcript
