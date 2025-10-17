0e8df3d0ff48252e3662852be126aa3b77d1198b
Billabong1985 <124009245+Billabong1985@users.noreply.github.com>
2025-10-17 13:50:47 +0100

    Create DetectLogFileString.ps1

---
+++ #Templates/DetectLogFileString.ps1
@@ -0,0 +1,35 @@
+# Define the log folder and file name
+$LogFolder = "C:\Software\StartMenu"
+$LogFile = "$LogFolder\StartMenuLayout.log"
+
+# Set an initial success flag of false
+$Success = $false
+
+# Set 1 or more strings to search for in the log file
+$SearchString = "String A", "String B"
+
+# Search the log file for the expected string(s)
+$Search = Select-String -Path $LogFile -Pattern $SearchString
+
+<# Choose one of the below depending on whether you searched for a string that indicate success,
+or the absense of strings indicating failure
+
+# OPTION 1 - If the search DID find the specified string(s) indicating success, change the flag to true
+If ($null -ne $StringSearch) {
+    $Success = $true
+}
+
+#OPTION 2 - If the search DID NOT find the specified string(s) indicating failure, change the flag to true
+If ($null -eq $StringSearch) {
+    $Success = $true
+}
+#>
+
+# If success flag has been set as true, output a success exit code, otherwise output a failure exit code
+if ($Success -eq $true) {
+    Write-host Success
+    Exit 0
+}
+Else {
+    Exit 1
+}
