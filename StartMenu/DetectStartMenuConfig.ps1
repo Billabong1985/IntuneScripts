217d229c7b0a87f3ee65fac502f49d5f413384c9
Billabong1985 <124009245+Billabong1985@users.noreply.github.com>
2025-10-17 13:55:07 +0100

    Update DetectStartMenuConfig.ps1

--- StartMenu/DetectStartMenuConfig.ps1
+++ StartMenu/DetectStartMenuConfig.ps1
@@ -1,10 +1,38 @@
+# Define variables
 $CurrentUser = (Get-CimInstance -ClassName Win32_ComputerSystem | Select-Object -ExpandProperty UserName).Split('\')[-1]
+$WinVer = (Get-CimInstance -ClassName Win32_OperatingSystem).Caption
 $LogFolder = "C:\Software\StartMenu"
-$LogFile = "$LogFolder\StartMenuConfig$CurrentUser.log"
-$Failure = "incorrect value","not present"
-$Status = Get-Content $LogFile | select-string ($Failure)
+$Success = $false
 
-if($null -eq $Status)
-    {
-    write-host Success
-    }
+# Define the expected log file if the OS is Windows 10
+if ($WinVer -like '*Windows 10*') {
+    $LogFile = "$LogFolder\Win10StartMenuConfig$CurrentUser.log"
+}
+
+# Define the expected log file if the OS is Windows 11
+if ($WinVer -like '*Windows 11*') {
+    $LogFile = "$LogFolder\Win11StartMenuConfig$CurrentUser.log"
+}
+
+# Check whether the log file exists, exit with fail code if not
+If ((Test-Path $LogFile) -eq $False) {
+    Write-Host "Log file not found"
+    Exit 1
+}
+# Check for keywords indicating failures
+$FailureKeywords = "incorrect value", "not present"
+$KeywordSearch = Select-String -Path $LogFile -Pattern $FailureKeywords
+
+# If no failure keywords were found, change the success flag to true
+If ($null -eq $KeywordSearch) {
+    $Success = $true
+}
+
+# Success has been set as true, output a success exit code, otherwise output a failure exit code
+if ($Success -eq $true) {
+    Write-host Success
+    Exit 0
+}
+Else {
+    Exit 1
+}
